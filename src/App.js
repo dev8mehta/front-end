fd49988803d685c3e574740ac7d61bf55380ae0d
pjmich <pmichaud@umass.edu>
2024-10-21 17:06:11 -0400

    ADDED: light /dark mode character counter and limit ctrl enter functionality
    FIXED:
    Some minor formatting bugs
    When you pressed enter to submit a question it would make a new line in the chat bubble
    there was prolly sumn else im forgetting

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import './App.css';
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+
+//Char Limit
+const maxLength = 1000;
+
+//THEMES
+const lightTheme = {
+  background: "#ffffff",
+  textColor: "#000000",
+  headerBackground: "#2394d9",
+  messageBackgroundUser: "#2394d9",
+  messageBackgroundBot: "#e9ecef",
+};
+
+const darkTheme = {
+  background: "#1a1a1a",
+  textColor: "#ffffff",
+  headerBackground: "#2394d9",
+  messageBackgroundUser: "#2394d9",
+  messageBackgroundBot: "#555555",
+};
 
 const ChatContainer = styled.div`
-  paddingLeft: 50px;
+  paddingleft: 50px;
   border: 1px solid #ddd;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  background: #1a1a1a;
+  background: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.textColor};
   height: 100vh;
 `;
 
 const Header = styled.div`
-  background: #2394d9;
+  background: ${(props) => props.theme.headerBackground};
   color: white;
   padding: 20px;
   text-align: center;
   font-family: Arial, Helvetica, sans-serif;
   font-weight: bold;
-  font-size:2vw;
+  font-size: 2vw;
+  position: relative;
+`;
+//This is just for the title of the website because I wanted the switch themes button
+//up top with it but not centered
+const Title = styled.div`
+  text-align: center;
 `;
 
 const MessagesContainer = styled.div`
@@ -36,16 +63,20 @@ const MessagesContainer = styled.div`
 `;
 
 const Message = styled.div`
-  background: ${(props) => (props.isBot ? '#e9ecef' : '#2394d9')};
-  color: ${(props) => (props.isBot ? '#000' : '#fff')};
+  background: ${(props) =>
+    props.isBot
+      ? props.theme.messageBackgroundBot
+      : props.theme.messageBackgroundUser};
+  color: ${(props) => (props.isBot ? props.theme.textColor : "#fff")};
   padding: 10px;
   border-radius: 10px;
   margin: 5px 0;
-  align-self: ${(props) => (props.isBot ? 'flex-start' : 'flex-end')};
-  float: ${(props) => (props.isBot ? 'left' : 'right')};
+  align-self: ${(props) => (props.isBot ? "flex-start" : "flex-end")};
+  float: ${(props) => (props.isBot ? "left" : "right")};
   overflow-wrap: break-word;
   max-width: 75%;
   font-family: Arial, Helvetica, sans-serif;
+  white-space: pre-wrap;
 `;
 
 const InputContainer = styled.div`
@@ -64,7 +95,7 @@ const TextInput = styled.textarea`
 `;
 
 const SendButton = styled.button`
-  background: #2394d9;
+  background: ${(props) => props.theme.headerBackground};
   color: white;
   padding: 15px 20px;
   border: none;
@@ -72,48 +103,105 @@ const SendButton = styled.button`
   outline: none;
 `;
 
+const CharCount = styled.div`
+  text-align: center;
+  color: ${(props) => (props.remaining < 0.05 * maxLength ? "red" : "green")};
+  font-size: 12px;
+  padding: 10px 1px;
+  background: ${(props) => props.theme.background};
+`;
+
+const ThemeToggleButton = styled.button`
+  background: none;
+  border: none;
+  color: white;
+  font-size: 1em;
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+`;
+
 const App = () => {
   const [messages, setMessages] = useState([
-    { text: 'Hello! How can I assist you today?', isBot: true },
+    { text: "Hello! How can I assist you today?", isBot: true },
   ]);
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
+  const [theme, setTheme] = useState(darkTheme);
 
   // Handle sending a new message
   const handleSendMessage = () => {
     if (userInput.trim()) {
       const userMessage = { text: userInput, isBot: false };
       setMessages([userMessage, ...messages]);
-      setUserInput('');
-      
+      setUserInput("");
+
       // Simulate bot response
       setTimeout(() => {
-        const botMessage = { text: 'I am a function, not an AI. This is pre-written text.', isBot: true };
+        const botMessage = {
+          text: "I am a function, not an AI. This is pre-written text.",
+          isBot: true,
+        };
         setMessages((prevMessages) => [botMessage, ...prevMessages]);
       }, 1000); // Response delay to simulate real interaction
     }
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (e.ctrlKey) {
+        // Ctrl + Enter makes new line
+        const cursorPos = e.target.selectionStart;
+        setUserInput(
+          userInput.slice(0, cursorPos) + "\n" + userInput.slice(cursorPos)
+        );
+        setTimeout(() => {
+          e.target.selectionStart = e.target.selectionEnd = cursorPos + 1;
+        }, 0);
+      } else {
+        e.preventDefault();
+        handleSendMessage();
+      }
+    }
+  };
+
+  const toggleTheme = () => {
+    setTheme(theme === lightTheme ? darkTheme : lightTheme);
+  };
+
   return (
-    <ChatContainer>
-      <Header>ISOtope's RAGatouille Chatbot</Header>
-      <MessagesContainer>
-        {messages.map((msg, index) => (
-          <Message key={index} isBot={msg.isBot}>
-            {msg.text}
-          </Message>
-        ))}
-      </MessagesContainer>
-      <InputContainer>
-        <TextInput
-          type="text"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          placeholder="Type your message..."
-          onKeyPress={(e) => e.key === 'Enter' && handleSendMessage()}
-        />
-        <SendButton onClick={handleSendMessage}>Send</SendButton>
-      </InputContainer>
-    </ChatContainer>
+    <ThemeProvider theme={theme}>
+      <ChatContainer>
+        <Header>
+          <Title>ISOtope's RAGatouille Chatbot</Title>
+          <ThemeToggleButton onClick={toggleTheme}>
+            {theme === darkTheme ? "🌙 Dark Mode" : "☀️ Light Mode"}
+          </ThemeToggleButton>
+        </Header>
+        <MessagesContainer>
+          {messages.map((msg, index) => (
+            <Message key={index} isBot={msg.isBot}>
+              {msg.text}
+            </Message>
+          ))}
+        </MessagesContainer>
+        <InputContainer>
+          <TextInput
+            type="text"
+            maxLength={maxLength}
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder="Type your message..."
+            onKeyDown={handleKeyPress}
+          />
+          <CharCount remaining={maxLength - userInput.length}>
+            {maxLength - userInput.length} Characters Remaining
+          </CharCount>
+          <SendButton onClick={handleSendMessage}>Send</SendButton>
+        </InputContainer>
+      </ChatContainer>
+    </ThemeProvider>
   );
 };
 
